cc6ec3ef6294f6e3a0eb505b22c38ee15e5f3884
sue445 <sue445@sue445.net>
2018-08-05 16:51:29 +0900

    Add nginx_spec

---
+++ spec/nginx_spec.rb
@@ -0,0 +1,10 @@
+require_relative "spec_helper"
+
+describe package("nginx") do
+  it { should be_installed }
+end
+
+describe service("nginx") do
+  it { should be_enabled }
+  it { should be_running }
+end
